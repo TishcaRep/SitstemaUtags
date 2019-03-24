40bed296e821ac929509de88d0db209e27c6a852
Tishca Dev <tishcadev@gmail.com>
2019-03-23 19:57:36 -0600

    rutas igresar/registrar

---
+++ app/ajax/login/registrar.php
@@ -0,0 +1,12 @@
+<?php
+if (empty($_POST['datos'])) {
+  tojson(['error'=>TRUE,'message'=>'No se resivieron los datos necesarios']);
+}
+$datos = $_POST['datos'];
+$res=register($datos['Correo_Electronico'], $datos['Contraseña'],'user' ,"",false,[]);
+if ($res['error']) {
+  tojson(['error'=>TRUE,'message'=>'El correo ya esta registrado']);
+}
+else {
+  tojson(['error'=>FALSE,'message'=>'Bienvenido']);
+}
